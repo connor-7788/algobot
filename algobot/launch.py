284528e197b41e697a1555731ef49eed6679f8c9
connor-7788 <281074995+connor-7788@users.noreply.github.com>
2026-05-03 17:46:45 -0400

    Delete algobot directory

--- algobot/launch.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-def start_engine():
-    print("Initializing AlgoTrader v2.2...")
-    subprocess.run(["python", "cross_asset_trader.py"])
-
-if __name__ == "__main__":
-    start_engine()
